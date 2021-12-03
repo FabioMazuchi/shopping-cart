0ef2ef50e4c8c4a9a1272cd0ec293d1b6183d7dc
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-12-03 10:09:37 -0300

    adiciona requisito 1 e 8

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (produto = 'computador') => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
+
+  try {
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;  
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -40,4 +40,11 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+window.onload = async () => {
+  const res = await fetchProducts();
+  const section = document.querySelector('.items');
+  console.log(section);
+  res.results.forEach((element) => {
+    section.appendChild(createProductItemElement(element));
+  });
+};
