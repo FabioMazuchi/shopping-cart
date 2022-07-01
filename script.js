87d5e8d684e5c6f189b59a4c3b73aa25cfea27ed
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-07-01 08:47:57 -0300

    remove arquivos

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const ol = document.querySelector('.cart__items');
 const somaSpan = document.querySelector('.total-price');
 const esvaziarBtn = document.querySelector('.empty-cart');
+const cartIcon = document.getElementsByTagName('i');
+const carrinho = document.querySelector('.cart');
+const voltar = document.querySelector('.voltar');
+const circle = document.querySelector('.circle__cart');
+
+const removerCart = () => {
+  carrinho.style.display = 'none';
+}
+
+voltar.addEventListener('click', removerCart);
+
+const exibirCarrinho = () => {
+  carrinho.style.display = 'block';
+};
+
+cartIcon[0].addEventListener('click', exibirCarrinho);
 
 const somar = () => {
   const array = ol.children;
   let soma = 0;
   for (let i = 0; i < array.length; i += 1) {
-    const pos = array[i].innerText.indexOf('$');
-    const res = Number(array[i].innerText.substring(pos + 1));
-    soma += res;
+    soma += array[i].value;
   }
-  somaSpan.innerText = soma;
+  somaSpan.innerText = soma.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
 };
 
 const esvaziarCarrinho = () => {
@@ -18,9 +32,9 @@ const esvaziarCarrinho = () => {
 
   for (let i = 0; i < array.length; i += i) {
     array[i].remove();  
-    console.log(array.length);
   }
   somar();
+  addNumberOfIconCart();
 };
 
 esvaziarBtn.addEventListener('click', esvaziarCarrinho);
@@ -41,7 +55,7 @@ function createCustomElement(element, className, innerText) {
 
 function cartItemClickListener(event) {
   const array = ol.children;
-  const select = event.target;
+  const select = event.target.parentNode;
   select.classList.add('clicou');
 
   for (let i = 0; i < array.length; i += 1) {
@@ -50,13 +64,39 @@ function cartItemClickListener(event) {
     }  
   }
   somar();
+  addNumberOfIconCart();
 }
 
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice, thumbnail }) {
   const li = document.createElement('li');
+  // const span__sku = document.createElement('span');
+  const span__name = document.createElement('span');
+  const span__price = document.createElement('span');
+  // const p = document.createElement('p');
+  const i = document.createElement('i');
+  
+  const div = document.createElement('div');
+
+  li.appendChild(createProductImageElement(thumbnail));
+  
+  // span__sku.innerText = sku;
+  // div.appendChild(span__sku);
+  
+  span__name.innerText = name;
+  div.appendChild(span__name);
+ 
+  span__price.innerText = salePrice.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+  div.appendChild(span__price);
+
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.value = salePrice;
+  li.appendChild(div);
+
+  i.classList.add('fas');
+  i.classList.add('fa-times-circle');
+  li.appendChild(i);
+  
+  i.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -70,6 +110,8 @@ const buscarDadosProduto = async (id) => {
   ol.appendChild(li);
   saveCartItems(ol.innerHTML);
   somar();
+  addNumberOfIconCart();
+  alert('Produto adicionado com sucesso!')
 };
 
 const addListennerOnButtons = () => {
@@ -89,7 +131,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image, pric
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('span', 'item__price', `R$${price}`));
+  section.appendChild(createCustomElement('span', 'item__price', `${price.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   items.appendChild(section);
 }
@@ -119,6 +161,18 @@ const getLocalStorage = () => {
   somar();
 };
 
+const addNumberOfIconCart = () => {
+  const qtd = ol.childNodes.length;  
+  if (qtd === 0) {
+    circle.style.display = 'none';
+  } else {
+    circle.innerHTML = qtd;
+    circle.style.display = 'block';
+  }
+};
+
+addNumberOfIconCart();
+
 window.onload = async () => {
   addloading();
   await buscarDados();
@@ -126,4 +180,5 @@ window.onload = async () => {
   clearLoading();
   somar();
   getLocalStorage();
+  esvaziarCarrinho();
 };
